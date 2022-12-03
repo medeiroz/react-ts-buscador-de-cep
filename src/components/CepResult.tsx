8cad8938173d843846fc5b30c3069ae4957ad4b5
Flavio Medeiros <smedeiros.flavio@gmail.com>
2022-12-03 15:01:22 -0300

    Updated page title and remove rua prefix of components/CepResult.tsx

--- src/components/CepResult.tsx
+++ src/components/CepResult.tsx
@@ -8,7 +8,7 @@ const CepResult = (props: CepResultProps) => {
   return (
         <main className='flex flex-col justify-evenly bg-white rounded mx-8 py-4 px-8 max-w-lg md-w-3/4 w-full h-48'>
           <span>CEP: { props.cep.cep }</span>
-          <span>Rua: { props.cep.street }</span>
+          <span>{ props.cep.street }</span>
           <span>{ props.cep.neighborhood }</span>
           <span>{ props.cep.city } - { props.cep.state }</span>
         </main>
